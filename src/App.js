cf643f18284737ae7281a412bcaf0efe1fb8359c
Evan Powell <evan.powell@gmail.com>
2018-10-12 12:07:16 -0700

    Added consonant and fixed final text state

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
 
     this.state = {
       userText: "Something", //text from the user
-      finalText: "", //translated text
-      workingText: "", //temp storage for text
+      finalText: "final", //translated text
+      workingText: "working", //temp storage for text
       vowels: ["a", "e", "i", "o", "u"] //but never "y"
 
     }
@@ -31,29 +31,63 @@ class App extends Component {
   }
 
   handleTranslatedText = () => {
-    //this.setState({finalText: this.state.userText})
     this.pigLatify();
+
+    //Needed to update the text on the display
+     //this.setState({finalText: this.state.workingText})
+
+    console.log("userText " + this.state.userText);
+    console.log("workingText " + this.state.workingText);
+    console.log("finalText " + this.state.finalText);
 
   }
 
-  //this will compare all functions to update the final text
+  //This will compare all functions to update the working text
   //No return, just set state
   pigLatify(){
     if(this.isFirstVowel(this.state.userText)){
-      console.log("was true");
+      console.log("Vowel was true");
+      console.log("userText in PigIf: " + this.state.userText);
+      console.log("workingText in PigIf: " + this.state.workingText);
+      console.log("finalText PigIf: " + this.state.finalText);
+
       //push "-way" to the end of the word
-      this.setState({finalText: this.state.userText + "-way" })
+      this.setState({
+                      finalText: this.state.userText + "-way",
+                      workingText: this.state.userText + "-way"
+                    })
+
+    }else if (this.isConsonant(0)){
+      let i = 1
+      do{
+        this.isConsonant(i)
+        i++
+      }while(this.isConsonant(i))
+
 
     }else{
-      console.log("I'm in the else");
+      console.log("Invalid");
+      //this.setState({finalText: "INVALID"})
     }
+    //Needed to update the text on the display
+    //this.setState({finalText: this.state.workingText})
   }
 
   //takes in the user text to determine if the first letter is a vowel.
-  //will return bool
+  //Returns bool
   isFirstVowel(string){
 
     if(this.state.vowels.includes(string[0])){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  //Takes in the user text to determine if a letter is a consonant
+  //Returns bool
+  isConsonant(index){
+    if(this.state.vowels.includes(this.state.userText[index])){
       return true
     }else{
       return false
